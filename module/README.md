23b6bad83f94ca7667ddbb002f0bd1bd584bac97
no7dw <no7david@gmail.com>
2015-02-03 22:00:30 +0800

    add module

---
+++ module/README.md
@@ -0,0 +1,2 @@
+template code for public private simulate
+
---
+++ module/template.js
@@ -0,0 +1,19 @@
+var myNamespace = (function(){
+	var myPrivateVar = 0;
+	var myPrivateMethod = function(foo){
+		console.log(foo);
+		console.log(myPrivateVar);
+	};
+
+	return {
+		myPublicVar : "foo",
+		myPublicFunction: function (bar){
+			myPrivateVar++;
+			myPrivateMethod(bar);
+		}
+	};
+})();
+
+console.log(myNamespace.myPublicVar);
+myNamespace.myPublicFunction("ab");
+myNamespace.myPublicFunction("abcd");
