3963a7022540d4558831c50ad4bcc67df6a541ba
no7dw <no7david@gmail.com>
2017-02-19 16:59:45 +0800

    refactor for two case

--- constructor/polyOrder/orderUsage.js
+++ constructor/polyOrder/orderUsage.js
@@ -3,27 +3,31 @@
 const dOrder = require('./demandDepositOrder')
 const tOrder = require('./timeDepositOrder')
 
-//method1 从参数上判断
-let handleFactoryOrder = function(order){
+//method1 从参数上判断, 类似处理web req
+let handleFactoryOrder = function(reqOrder){
   let _order;
-  if(order.period){
-    _order = new tOrder(order)
+  if(reqOrder.period){
+    _order = new tOrder(reqOrder)
   }
   else {
-    _order = new dOrder(order)
+    _order = new dOrder(reqOrder)
   }
   _order.Confirm()
 }
-handleFactoryOrder( {number:10, rateYear : 0.071})
-handleFactoryOrder( {number:10, rateYear : 0.071, period:90})
+
+let req = {}
+req.body = {number:10, rateYear : 0.071}
+let req2 = {}
+req2.body = {number:10, rateYear : 0.071, period:90}
+
+handleFactoryOrder( req.body)
+handleFactoryOrder( req2.body)
+
 
 //method2 从外部直接区分具体类别
 let handleFactoryOrder2 = function(order){
   order.Confirm()
 }
 
-let req = {}
-req.body = {number:10, rateYear : 0.071}
-req.body1 = {number:10, rateYear : 0.071, period:90}
 handleFactoryOrder2( new dOrder(req.body))
-handleFactoryOrder2( new tOrder(req.body1))
+handleFactoryOrder2( new tOrder(req2.body))
